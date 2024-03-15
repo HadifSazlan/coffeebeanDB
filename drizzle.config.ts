edb8daf5134ebc3cdccd36d51243298581424f6b
Afiq Sazlan <afiqsazlan@gmail.com>
2024-03-15 15:27:25 +0800

    Added drizzle config and suppliers schema

---
+++ drizzle.config.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import type {Config} from 'drizzle-kit';
+
+const host = process.env.DB_HOST ?? 'localhost';
+const port = process.env.DB_PORT ?? '5432';
+const database = process.env.DB_DATABASE ?? 'your_database';
+const user = process.env.DB_USER ?? 'your_username';
+const password = process.env.DB_PASSWORD ?? 'your_password';
+
+const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`;
+
+const config: Config = {
+    schema: './db/schema/*',
+    out: './db/migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: connectionString
+    },
+}
+
+export default config;

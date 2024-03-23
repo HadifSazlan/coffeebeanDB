e82d4785747fb946aee4868c3a2f88416ddc5292
Afiq Sazlan <afiqsazlan@gmail.com>
2024-03-23 15:14:34 +0800

    Fixed query for list of beans

--- src/domains/relation/routes.js
+++ src/domains/relation/routes.js
@@ -1,10 +1,9 @@
 import {Router} from 'express';
 
 import {index} from './controller.js';
-import {authenticateJWT} from "../../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.get('/', authenticateJWT, index);
+router.get('/', index);
 
 export default router;

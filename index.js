c056000615b8c5eb31b0fba95df960e6f3f49b3b
Afiq Sazlan <afiqsazlan@gmail.com>
2024-04-12 21:43:26 +0800

    Updated index.js to use dynamic PORT, dependent on .env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import passport from "passport";
 import './src/config/passport.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(passport.initialize());
 

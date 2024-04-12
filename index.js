d0cdf66409071b69230fe7a4f19100c0050b2239
Afiq Sazlan <afiqsazlan@gmail.com>
2024-04-12 09:29:26 +0800

    Moved index.js to root

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import authRoutes from './domains/admin/auth/routes.js';
-import adminRouter from './routes/admin.js'
-import guestRouter from './routes/guest.js'
+import authRoutes from './src/domains/admin/auth/routes.js';
+import adminRouter from './src/routes/admin.js'
+import guestRouter from './src/routes/guest.js'
 
 import passport from "passport";
-import './config/passport.js';
+import './src/config/passport.js';
 
 const app = express();
 const PORT = 3000;
@@ -19,4 +19,4 @@ app.use('/api/v1/', authRoutes);
 app.use('/api/v1', guestRouter);
 app.use('/api/v1/admin', adminRouter);
 
-app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));

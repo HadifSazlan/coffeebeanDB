aa01e36e07c3bb9f31ceb077266a32d434da830c
Afiq Sazlan <afiqsazlan@gmail.com>
2024-05-22 08:58:47 +0800

    Implemented CORS to restrict api access

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import authRoutes from './src/domains/admin/auth/routes.js';
 import adminRouter from './src/routes/admin.js'
 import guestRouter from './src/routes/guest.js'
+import cors from 'cors'
 
 import passport from "passport";
 import './src/config/passport.js';
@@ -11,6 +12,18 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(passport.initialize());
+
+const whitelist = [process.env.FE_URL];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Are you lost? Fret not. With hardship, comes ease.'))
+        }
+    }
+}
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 

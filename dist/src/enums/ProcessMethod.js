6ef101e57a2e53af99af8f6086ee52730f9fed2a
Afiq Sazlan <afiqsazlan@gmail.com>
2024-06-01 19:29:39 +0800

    Deleted dist folder contents

--- dist/src/enums/ProcessMethod.js
+++
@@ -1,9 +0,0 @@
-export var ProcessMethod;
-(function (ProcessMethod) {
-    ProcessMethod["WASHED"] = "washed";
-    ProcessMethod["SEMI_WASHED"] = "semi-washed";
-    ProcessMethod["FULLY_WASHED"] = "fully-washed";
-    ProcessMethod["NATURAL"] = "natural";
-    ProcessMethod["ANAEROBIC_NATURAL"] = "anaerobic natural";
-    ProcessMethod["JADE_WET_HULLED"] = "jade wet-hulled";
-})(ProcessMethod || (ProcessMethod = {}));
--- dist/src/enums/RoastType.js
+++
@@ -1,8 +0,0 @@
-export var RoastType;
-(function (RoastType) {
-    RoastType["LIGHT"] = "light";
-    RoastType["MEDIUM_LIGHT"] = "medium-light";
-    RoastType["MEDIUM"] = "medium";
-    RoastType["MEDIUM_DARK"] = "medium-dark";
-    RoastType["DARK"] = "dark";
-})(RoastType || (RoastType = {}));
